55a1a24887ab9116ecd6cd8f9e2d49f52079644f
chermarya <chermarya25@gmail.com>
2024-06-26 12:56:19 +0300

    DbHelper

--- app/src/main/java/com/example/biddecor/MainActivity.kt
+++ app/src/main/java/com/example/biddecor/MainActivity.kt
@@ -29,7 +29,7 @@ class MainActivity : AppCompatActivity() {
                 R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications
             )
         )
-        //TEST
+
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
     }
